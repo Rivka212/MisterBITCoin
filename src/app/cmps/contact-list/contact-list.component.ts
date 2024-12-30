913251b4643c715046127d80372f0fa971b8c8c4
Bashari Rivka <basrivka1@gmail.com>
2024-12-30 13:23:13 +0200

    Add contact list cmp

---
+++ src/app/cmps/contact-list/contact-list.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Contact } from '../../models/contact.model';
+
+@Component({
+  selector: 'contact-list',
+  standalone: false,
+
+  templateUrl: './contact-list.component.html',
+  styleUrl: './contact-list.component.scss'
+})
+export class ContactListComponent {
+  @Input() contacts: Contact[] | null = null
+}
